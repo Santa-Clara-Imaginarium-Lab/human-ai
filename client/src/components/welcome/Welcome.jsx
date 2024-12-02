a5450324f8555fa38352c7bca041bc7cb83d7788
Ethan Sychangco <esychangco@gmail.com>
2024-12-01 21:03:13 -0800

    30 case switch statement for changing color combo

--- client/src/components/welcome/Welcome.jsx
+++ client/src/components/welcome/Welcome.jsx
@@ -56,9 +56,112 @@ const Welcome = ({changeTheme}) => {
         };
     }, []);
 
+    function handleEnter (e) {
+        if (e.key === 'Enter') {
+            const currentText = e.target.value;
+            switch (currentText) {
+                case '1 1':
+                    changeTheme('yellow-blue-theme-1-1');
+                    break;
+                case '1 2':
+                    changeTheme('yellow-blue-theme-1-2');
+                    break;
+                case '1 3':
+                    changeTheme('yellow-blue-theme-1-3');
+                    break;
+                case '1 4':
+                    changeTheme('yellow-blue-theme-1-4');
+                    break;
+                case '1 5':
+                    changeTheme('yellow-blue-theme-1-5');
+                    break;
+                case '1 6':
+                    changeTheme('yellow-blue-theme-1-6');
+                    break;
+                case '1 7':
+                    changeTheme('yellow-blue-theme-1-7');
+                    break;
+                case '1 8':
+                    changeTheme('yellow-blue-theme-1-8');
+                    break;
+                case '1 9':
+                    changeTheme('yellow-blue-theme-1-9');
+                    break;
+                case '2 1':
+                    changeTheme('red-green-theme-2-1');
+                    break;
+                case '2 2':
+                    changeTheme('red-green-theme-2-2');
+                    break;
+                case '2 3':
+                    changeTheme('red-green-theme-2-3');
+                    break;
+                case '2 4':
+                    changeTheme('red-green-theme-2-4');
+                    break;
+                case '2 5':
+                    changeTheme('red-green-theme-2-5');
+                    break;
+                case '2 6':
+                    changeTheme('red-green-theme-2-6');
+                    break;
+                case '2 7':
+                    changeTheme('red-green-theme-2-7');
+                    break;
+                case '2 8':
+                    changeTheme('red-green-theme-2-8');
+                    break;
+                case '2 9':
+                    changeTheme('red-green-theme-2-9');
+                    break;
+                case '3 1':
+                    changeTheme('purple-turquoise-theme-3-1');
+                    break;
+                case '3 2':
+                    changeTheme('purple-turquoise-theme-3-2');
+                    break;
+                case '3 3':
+                    changeTheme('purple-turquoise-theme-3-3');
+                    break;
+                case '3 4':
+                    changeTheme('purple-turquoise-theme-3-4');
+                    break;
+                case '3 5':
+                    changeTheme('purple-turquoise-theme-3-5');
+                    break;
+                case '3 6':
+                    changeTheme('purple-turquoise-theme-3-6');
+                    break;
+                case '3 7':
+                    changeTheme('purple-turquoise-theme-3-7');
+                    break;
+                case '3 8':
+                    changeTheme('purple-turquoise-theme-3-8');
+                    break;
+                case '3 9':
+                    changeTheme('purple-turquoise-theme-3-9');
+                    break;
+                case '4 1':
+                    changeTheme('black-white-theme-4-1');
+                    break;
+                case '4 2':
+                    changeTheme('black-white-theme-4-2');
+                    break;
+                case '4 3':
+                    changeTheme('black-white-theme-4-3');
+                    break;    
+                default:
+                    console.log(currentText + " theme not found, defaulting to yellow-blue-theme-1-1");
+                    changeTheme('yellow-blue-theme-1-1');
+                    break;
+            }
+        }
+      }     
+
     return (
         <div className="container welcome">
-                        <div className="menu-container">
+        <div className="menu-container">
+        <input type="text" id="theme-input" onKeyDown={handleEnter} placeholder="{researcher code} {combo}" />
         <button className="menu-button" onClick={toggleMenu}>
           ☰
         </button>

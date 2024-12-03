96f77e91f326ea84e5dd124be3e238895f6cd2d3
joshsj89 <joshsj89@gmail.com>
2024-12-03 01:16:36 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { // Development server configuration
+    port: 5173,
+  },
+  preview: { // Production server configuration
+    port: 5173,
+  }
 })

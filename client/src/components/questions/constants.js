27dab209addcb48b0b3fc679ae4b60a6ab048319
joshsj89 <joshsj89@gmail.com>
2024-12-04 10:57:43 -0800

    Fix typo in survey

--- client/src/components/questions/constants.js
+++ client/src/components/questions/constants.js
@@ -5,7 +5,7 @@ const constants = [
     "I want to know that I understand this AI system correctly.",
     "I want to understand what the AI will do next.",
     "I want to know why the AI did not make some other decision.",
-    "I want to know why the AI would have done if something had been different.",
+    "I want to know what the AI would have done if something had been different.",
     "I was surprised by the AI's actions and want to know what I missed."
   ];
   

e7ba0bada0625296cd2735ee6b1ebd15e14283f6
joshsj89 <joshsj89@gmail.com>
2025-01-23 03:53:45 -0800

    Remove code for chatbots 2-4

--- client/src/routes/endScreen/EndScreen.jsx
+++ client/src/routes/endScreen/EndScreen.jsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from 'react';
 import './endScreen.css';
 
 const EndScreen = () => {
-    const [gameLogs, setGameLogs] = useState([]);
+    const [gameLog, setGameLog] = useState(null);
+    let totalChatbotScore = 0;
+    let totalUserScore = 0;
 
     useEffect(() => {
-        const gameLog1Info = JSON.parse(sessionStorage.getItem('gameLog1'));
-        const gameLog2Info = JSON.parse(sessionStorage.getItem('gameLog2'));
-        const gameLog3Info = JSON.parse(sessionStorage.getItem('gameLog3'));
-        const gameLog4Info = JSON.parse(sessionStorage.getItem('gameLog4'));
-        const gameLog5Info = JSON.parse(sessionStorage.getItem('gameLog5'));
+        const gameLogInfo = JSON.parse(sessionStorage.getItem('gameLog1'));
 
-        setGameLogs([gameLog1Info, gameLog2Info, gameLog3Info, gameLog4Info, gameLog5Info]);
+        setGameLog(gameLogInfo);
+
+        console.log(gameLogInfo);
     }, []);
 
     return (
@@ -36,34 +36,28 @@ const EndScreen = () => {
                     <p className="round-results-text">R5</p>
                 </div>
                 <div className="chatbot-results-wrapper">
-                    {gameLogs.map((gameLog, index) => {
-                        let totalChatbotScore = 0;
-                        let totalUserScore = 0;
-                        return (
-                            <div className="chatbot-results-rectangle" key={index}>
-                                <p className="chatbot-results-text">Chatbot {index + 1}</p>
-                                <div className="scores-labels-container">
-                                    <p className="chatbot-score-label">Chatbot's Score:</p>
-                                    <p className="player-score-label">Your Score:</p>
-                                </div>
-                                <div className="scores-container">
-                                    {gameLog && gameLog.data.map((gameLogData, index) => {
-                                        totalChatbotScore += gameLogData[`Round${index + 1}`].AI;
-                                        totalUserScore += gameLogData[`Round${index + 1}`].You;
-                                        return (
-                                            <div className={`scores-${index + 1}`} key={index}>
-                                                <p className="chatbot-score">{totalChatbotScore}</p>
-                                                <p className="player-score">{totalUserScore}</p>
-                                            </div>
-                                        );
-                                    })}
-                                </div>
-                                {totalChatbotScore < totalUserScore && 
-                                    <p className="chatbot-victory-text">VICTORY !!</p>
-                                }
-                            </div>
-                        );
-                    })}
+                    <div className="chatbot-results-rectangle">
+                        <p className="chatbot-results-text">Chatbot 1</p>
+                        <div className="scores-labels-container">
+                            <p className="chatbot-score-label">Chatbot's Score:</p>
+                            <p className="player-score-label">Your Score:</p>
+                        </div>
+                        <div className="scores-container">
+                            {gameLog && gameLog.data.map((gameLogData, index) => {
+                                totalChatbotScore += gameLogData[`Round${index + 1}`].AI;
+                                totalUserScore += gameLogData[`Round${index + 1}`].You;
+                                return (
+                                    <div className={`scores-${index + 1}`} key={index}>
+                                        <p className="chatbot-score">{totalChatbotScore}</p>
+                                        <p className="player-score">{totalUserScore}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        {totalChatbotScore < totalUserScore && 
+                            <p className="chatbot-victory-text">VICTORY !!</p>
+                        }
+                    </div>
                 </div>
             </div>
         </div>

1d20f57e4ef521695d33ebe6d83c13a33128ae08
Ethan Sychangco <esychangco@gmail.com>
2025-02-26 16:07:40 -0800

    apply research font in pre-survey

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -61,7 +61,7 @@ function Login({changeTheme, changePersonality}) {
             <p>
               <br/>
               <strong>Note: </strong>
-              Your participant ID has been provided via the researcher. <br/> If you are taking this study remotely, it is available in the study invitation email. <br/><br/> For any questions or further assistance, please notify the researcher.
+              Your participant ID has been provided via the researcher. If you are taking this study remotely, it is available in the study invitation email. <br/><br/> For any questions or further assistance, please notify the researcher.
             </p>
           </div>
         </div>

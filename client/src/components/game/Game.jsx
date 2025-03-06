d00df9c143dc0b2d7ebdd6b49c4835d9c20fff4c
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2025-03-06 11:44:25 -0800

    wrong api endpoint

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -52,7 +52,7 @@ function Game() {
   
   const mutation = useMutation({
       mutationFn: () => {
-        return fetch(`${import.meta.env.VITE_API_URL}/api/chats/${data._id}`, {
+        return fetch(`https://human-ai.up.railway.app/api/chats/${data._id}`, {
           method: "PUT",
           credentials: "include",
           headers: {
